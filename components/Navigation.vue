f22ec925319fd9548d3dd36012d844414302350a
lucas-cq <lucas.cummings024@gmail.com>
2021-12-08 17:10:27 -0700

    nav done

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -3,7 +3,7 @@
     <nav>
       <ul>
         <div class="img-wrapper">
-        <NuxtLink to="/"><svg class="main-logo" width="60" height="60" viewBox="0 0 380 380" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <NuxtLink to="/"><svg class="main-logo" width="80" height="80" viewBox="0 0 380 380" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path class="main-logo" fill-rule="evenodd" clip-rule="evenodd" d="M199.628 380C300.088 374.983 380 291.89 380 190.12C380 85.1196 294.934 0 190 0C179.792 0 169.772 0.805527 160 2.35652V340.477L297 260.252V322.98L199.628 380ZM190 92.0517L289 41.0259V88.904L230.308 119.154V216.618L296 178.922V225.062L233.294 261.045L233.308 261.045L232.855 261.297L230.207 262.816L230.187 262.781L190.216 285.015L190.305 262.215L190.308 262.215V92.5349L190 92.0517Z" fill="#FF5D73"/>
         <path class="main-logo" d="M109 18.0927C44.5776 48.5179 0 114.11 0 190.12C0 266.13 44.5776 331.722 109 362.147V18.0927Z" fill="#FF5D73"/>
         </svg></NuxtLink>
@@ -31,8 +31,8 @@
   }
 
   nav li {
-  margin: 20px 0px 0px 50px;
-  font-size: 20px;
+  margin: 25px 0px 0px 50px;
+  font-size: 27px;
   list-style: none;
   }
   
@@ -47,7 +47,7 @@
   }
 
   .main-logo {
-    width: 60px;
+    width: 70px;
     filter: drop-shadow(0px 1.5px 2px #212124);
     transition: 0.5s;
   }
@@ -57,8 +57,8 @@
   }
 
   .contact-button {
-    margin: 30px 13rem auto auto;
-    font-size: 20px;
+    margin: 35px 13rem auto auto;
+    font-size: 27px;
     background-color: #FF5D73;
     border-radius: 10px;
     padding: 5px;
@@ -68,6 +68,7 @@
 
   .contact-button:hover {
     background-color: #ff3d58;
+    transition: 0.5s;
   }
   
 </style>
