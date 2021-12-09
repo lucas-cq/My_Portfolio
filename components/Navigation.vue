e59a9a0d498f3622058bb06066dba094cce26abb
lucas-cq <lucas.cummings024@gmail.com>
2021-12-09 14:13:29 -0700

    added nav

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,25 +1,25 @@
 <template>
   <header>
-    <nav>
-      <ul>
+    <nav class="nav-box">
+      <ul class="nav-flex">
         <div class="img-wrapper">
         <NuxtLink to="/"><svg class="main-logo" width="80" height="80" viewBox="0 0 380 380" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path class="main-logo" fill-rule="evenodd" clip-rule="evenodd" d="M199.628 380C300.088 374.983 380 291.89 380 190.12C380 85.1196 294.934 0 190 0C179.792 0 169.772 0.805527 160 2.35652V340.477L297 260.252V322.98L199.628 380ZM190 92.0517L289 41.0259V88.904L230.308 119.154V216.618L296 178.922V225.062L233.294 261.045L233.308 261.045L232.855 261.297L230.207 262.816L230.187 262.781L190.216 285.015L190.305 262.215L190.308 262.215V92.5349L190 92.0517Z" fill="#FF5D73"/>
         <path class="main-logo" d="M109 18.0927C44.5776 48.5179 0 114.11 0 190.12C0 266.13 44.5776 331.722 109 362.147V18.0927Z" fill="#FF5D73"/>
         </svg></NuxtLink>
         </div>
-        <li><NuxtLink class="nav-text desktop-nav" to="css">CSS</NuxtLink></li>
-        <li><NuxtLink class="nav-text desktop-nav" to="js">JS</NuxtLink></li>
-        <li><NuxtLink class="nav-text desktop-nav" to="api">API</NuxtLink></li>
+        <li v-show="!mobile"><NuxtLink class="nav-text desktop-nav" to="css">CSS</NuxtLink></li>
+        <li v-show="!mobile"><NuxtLink class="nav-text desktop-nav" to="js">JS</NuxtLink></li>
+        <li v-show="!mobile"><NuxtLink class="nav-text desktop-nav" to="api">API</NuxtLink></li>
       </ul>
-        <p class="contact-button desktop-nav"><NuxtLink class="nav-text desktop-nav" to="contact">Contact</NuxtLink></p>
+        <div @click="toggleMobileNav" v-show="mobile" class="hamburger" :class="{'hamburger open' : mobileNav}">
+          <span class="bar"></span>
+          <span class="bar"></span>
+          <span class="bar"></span>
+        </div>
+        <p v-show="!mobile" class="contact-button desktop-nav"><NuxtLink class="nav-text desktop-nav" to="contact">Contact</NuxtLink></p>
     </nav>
-      <div class="hamburger">
-      <span class="bar"></span>
-      <span class="bar"></span>
-      <span class="bar"></span>
-    </div>
-    <nav class="mobile-nav">
+    <nav v-show="mobileNav" class="mobile-nav" :class="{'mobile-nav open' : toggleMobileNav }">
       <ul>
         <li><NuxtLink class="nav-text" to="css">CSS</NuxtLink></li>
         <li><NuxtLink class="nav-text" to="js">JS</NuxtLink></li>
@@ -27,34 +27,55 @@
         <li><NuxtLink class="nav-text" to="contact">Contact</NuxtLink></li>
       </ul>
     </nav>
-    <script>
-      const hamburger = document.querySelector('.hamburger');
-      const mobileNav = document.querySelector('.mobile-nav');
-
-      const openNav = function () {
-        hamburger.classList.toggle('open');
-        mobileNav.classList.toggle('open');
-      }
-
-      hamburger.addEventListener('click', openNav);
-    </script>
   </header>
 </template>
 
+<script>
+export default {
+  name: 'navigation',
+  data() {
+    return {
+      mobile: null,
+      mobileNav: null,
+      windowWidth: null,
+    }
+  },
+  mounted() {
+    window.addEventListener('resize', this.checkScreen);
+    this.checkScreen();
+  },
+  methods: {
+    toggleMobileNav() {
+      this.mobileNav = !this.mobileNav
+    },
+
+    checkScreen() {
+      this.windowWidth = window.innerWidth
+      if (this.windowWidth <= 937){
+        this.mobile = true
+        return
+      }
+      this.mobile = false
+      this.mobileNav = false
+      return
+    }
+  }
+}
+</script>
+
 <style scoped>
-  nav {
+  .nav-box {
     display: flex;
-    justify-content: flex-start;
     margin-left: 12rem;
   }
 
-  nav ul {
+  .nav-box ul {
     display: flex;
     justify-content: space-around;
     flex-wrap: wrap;
   }
 
-  nav li {
+  .nav-box li {
   margin: 25px 0px 0px 50px;
   font-size: 27px;
   list-style: none;
@@ -95,12 +116,13 @@
     transition: 0.5s;
   }
 
-    /* ********** */
+  /* ********** */
   /* Mobile Nav */
   /* ********** */
 
   .mobile-nav {
     display: none;
+    transition: 1s ease all;
   }
 
   .hamburger {
@@ -111,9 +133,11 @@
   .bar {
     display: block;
     width: 40px;
-    height: 8px;
+    height: 6px;
     margin: 8px auto;
-    transition: all 0.3s ease-in-out;
+    transition: 0.3s ease all;
+    border-radius: 15px;
+    filter: drop-shadow(0px 1px 2px #323235);
     background-color: #FF5D73;
   }
 
@@ -124,7 +148,11 @@
 
     .hamburger {
       display: block;
-      margin-top: 30px;
+      margin: 2rem 2rem 2rem auto;
+    }
+
+    .img-wrapper {
+      display: flex;
     }
 
     .hamburger.open .bar:nth-child(2){
@@ -132,25 +160,43 @@
     }
 
     .hamburger.open .bar:nth-child(1){
-      transform: translateY(25px) rotate(45deg);
+      transform: translateY(20px) rotate(45deg);
     }
 
     .hamburger.open .bar:nth-child(3){
-      transform: translateY(-6.5px) rotate(-45deg);
-    }
-
-    .mobile-nav{
-      display: none;
+      transform: translateY(-7.5px) rotate(-45deg);
     }
 
     .mobile-nav.open {
-      position: absolute;
+      position: fixed;
       display: flex;
       flex-direction: column;
       text-align: center;
       width: 100%;
       margin-bottom: 20px;
       background-color: #7C7A7A;
+    }
+
+    .mobile-nav.open ul {
+      margin: auto 0px;
+      padding: 0px;
+    }
+
+    .mobile-nav.open li {
+      list-style-type: none;
+      font-size: 25px;
+      padding: 25px
+    }
+
+    .nav-box {
+      margin: 0 auto;
+    }
+
+    .nav-flex {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      flex-wrap: wrap;
     }
   }
   
