45ec03f4c1649d535c5892264b45d4873c0cef53
lucas-cq <lucas.cummings024@gmail.com>
2021-12-10 15:28:27 -0700

    update hero

--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,9 @@
   <main>
     <section class="hero-section">
       <div class="hero-text">
-        <h1><span class="lucas-text">Lucas</span> Wrote This</h1>
+        <h1>lucasCummings<span class="lucas-text">() => {</span></h1>
         <p>A modern website built by a web<br> developer at SAIT</p>
+        <h2>}</h2>
       </div>
       <svg class="main-logo" width="420" height="420" viewBox="0 0 380 380" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path class="main-logo" fill-rule="evenodd" clip-rule="evenodd" d="M199.628 380C300.088 374.983 380 291.89 380 190.12C380 85.1196 294.934 0 190 0C179.792 0 169.772 0.805527 160 2.35652V340.477L297 260.252V322.98L199.628 380ZM190 92.0517L289 41.0259V88.904L230.308 119.154V216.618L296 178.922V225.062L233.294 261.045L233.308 261.045L232.855 261.297L230.207 262.816L230.187 262.781L190.216 285.015L190.305 262.215L190.308 262.215V92.5349L190 92.0517Z" fill="#FF5D73"/>
@@ -42,8 +43,12 @@ body {
   font-size: 27px;
 }
 
+.hero-text p {
+  margin-bottom: 0px;
+}
+
 .hero-text h1{
-  font-size: 70px;
+  font-size: 65px;
   filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
   margin-bottom: 5px;
   width: 11ch;
@@ -86,6 +91,13 @@ body {
   height: 25rem;
   border-radius: 33px;
   margin: 5rem;
+}
+
+.hero-text h2 {
+  margin-top: 20px;
+  font-size: 65px;
+  color: #FF5D73;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
 }
 
 </style>
