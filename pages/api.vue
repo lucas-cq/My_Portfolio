b220a2b68a9be7e35eff0b7e0065d14542c82be5
lucas-cq <lucas.cummings024@gmail.com>
2021-12-12 13:33:15 -0700

    added insta api

--- pages/api.vue
+++ pages/api.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
     <Navigation />
+    <main>
+      <section class="article-section article-head">
+        <h1>Here are some of my <span class="lucas-text">API</span> skills</h1>
+      </section>
+      <section class="instagram-output">
+
+      </section>
+    </main>
     <Footer />
   </div>
 </template>
@@ -10,7 +18,18 @@
 </style>
 
 <script>
-export default {
-  
-}
+  const instagramApi = async () => { 
+  const response = await fetch('/.netlify/functions/instagram_api')
+  const data = await response.json()
+  const insta = data.find((item) => item.caption.includes('#coding'))
+  const displayPost = document.querySelector('.instagram-output') 
+   displayPost.innerHTML = `
+   <figure>
+   <img src='${insta.url}' alt="My post from instagram">
+   <p>${insta.caption.replace('#coding', '')}</p>
+   </figure>
+  ` 
+  console.log(data)
+  } 
+instagramApi() // Runs the API function on html
 </script>
