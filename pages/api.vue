3b063dde6198258e0c02f9813eb62dc9c197a1d7
lucas-cq <lucas.cummings024@gmail.com>
2021-12-12 17:18:05 -0700

    more tests

--- pages/api.vue
+++ pages/api.vue
@@ -24,7 +24,7 @@
     <figure>
     <img src='${insta.url}' alt="My post from instagram">
     <p class="handle">@lucas.nuxt</p>
-    <p class="caption">${insta.caption.filter(caption => !caption.startsWith('#'))}</p>
+    <p class="caption">${insta.caption.filter(!insta.caption.startsWith('#'))}</p>
     </figure>
     ` 
     console.log(data)
